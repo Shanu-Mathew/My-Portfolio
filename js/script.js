0f8129bc646564fa6be6d2b1d191179a600e17c1
Shanu Mathew <shanutmathew1703@gmail.com>
2023-07-26 16:14:09 +0530

    Completed HTML CSS(Made Website Responsive)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================Typing Animation ========================  */
+var typed=new Typed(".typing", {
+    strings:["Machine Learning Developer....","Data Scientist....","Software Engineer....","Data Analyst....","Web Developer....","Freelancer"],
+    typeSpeed:60,
+    BackSpeed:30,
+    loop:true
+})
